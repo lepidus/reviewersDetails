7f5ee463cd8cd2d59b06d4a581b1ca42fd9da2aa
Thiago Matos <thiago@lepidus.com.br>
2023-10-19 11:48:55 -0400

    Adds SelectReviewerListPanel component to reviewers tab
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#641
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

--- hookCallbacks/ReviewersTabHookCallback.inc.php
+++ hookCallbacks/ReviewersTabHookCallback.inc.php
@@ -9,6 +9,7 @@ class ReviewersTabHookCallback
         $this->plugin = $plugin;
 
         HookRegistry::register('Template::Settings::access', [$this, 'addReviewersTab']);
+        HookRegistry::register('TemplateManager::display', [$this, 'setupReviewersListPanel']);
     }
 
     public function addReviewersTab($hookName, $args)
@@ -16,6 +17,47 @@ class ReviewersTabHookCallback
         $templateMgr = $args[1];
         $output = &$args[2];
         $output .= $templateMgr->fetch($this->plugin->getTemplateResource('reviewersTab.tpl'));
+        return false;
+    }
+
+    public function setupReviewersListPanel($hookName, $args)
+    {
+        $templateMgr = $args[0];
+        $template = $args[1];
+        $request = Application::get()->getRequest();
+        $context = $request->getContext();
+
+        if ($template != 'management/access.tpl') {
+            return false;
+        }
+
+        AppLocale::requireComponents(LOCALE_COMPONENT_PKP_EDITOR);
+
+        $reviewerListPanel = new \PKP\components\listPanels\PKPSelectReviewerListPanel(
+            'reviewers',
+            __('user.role.reviewers'),
+            [
+                'apiUrl' => $request->getDispatcher()->url(
+                    $request,
+                    ROUTE_API,
+                    $context->getPath(),
+                    'users/reviewers'
+                ),
+                'getParams' => [
+                    'contextId' => $context->getId(),
+                ],
+                'selectorName' => 'reviewerId',
+            ]
+        );
+        $reviewerListPanel->set([
+            'items' => $reviewerListPanel->getItems($request),
+            'itemsMax' => $reviewerListPanel->getItemsMax(),
+        ]);
+
+        $components = $templateMgr->getState('components');
+        $components['reviewers'] = $reviewerListPanel->getConfig();
+        $templateMgr->setState(['components' => $components]);
+
         return false;
     }
 }
--- templates/reviewersTab.tpl
+++ templates/reviewersTab.tpl
@@ -1,2 +1,6 @@
 <tab id="reviewers" label="{translate key="user.role.reviewers"}">
+    <select-reviewer-list-panel
+        v-bind="components.reviewers"
+        @set="set"
+    ></select-reviewer-list-panel>
 </tab>
